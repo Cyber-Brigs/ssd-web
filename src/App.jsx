3b151a138f4fc3b74312bf2bfc4422db79beb5bb
steve <austines811@gmail.com>
2024-05-22 19:22:39 +0300

    creates an app component with routes

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { lazy, Suspense } from "react";
+import { Routes, Route, Outlet } from "react-router-dom";
+import "./components/styles/styles.css";
+// Base component
+import LandingPage from "./components/common/LandingPage";
+// User Authentication
+import SignUp from "./components/common/SignUp";
+import LogIn from "./components/common/LogIn";
+// Common utility Component
+import PageNotFound from "./components/common/PageNotFound";
+// import RequireAuth from "./components/common/utils/RequireAuth";
+import SpinLoading from "./components/common/utils/SpinLoading";
+
+// User Components
+import UserSidebar from "./components/users/UserSidebar";
+import UploadSrs from "./components/texts/UploadSrs";
+import TextPreProcessing from "./components/texts/TextPreProcessing";
+const UserDashboard = lazy(() => import("./components/users/UserDashboard"));
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Suspense fallback={<SpinLoading />}>
+        <Routes>
+          <Route path="*" element={<PageNotFound />} />
+          {/* COMMON ROUTES */}
+          <Route index element={<LandingPage />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/log-in" element={<LogIn />} />
+          {/* USER ROUTES */}
+          <Route path="user" element={<UserSidebar />}>
+            <Route index element={<UserDashboard />} />
+            <Route path="dashboard" element={<UserDashboard />} />
+            <Route path="uploads" element={<Outlet />}>
+              <Route index element={<UploadSrs />} />
+            </Route>
+            <Route path="preprocessing" element={<Outlet />}>
+              <Route index element={<TextPreProcessing />} />
+            </Route>
+            <Route path="lda-analysis" element={<Outlet />}>
+              {/* <Route index element={<LdaAnalysis />} /> */}
+            </Route>
+            <Route path="lsa-analysis" element={<Outlet />}>
+              {/* <Route index element={<LdaAnalysis />} /> */}
+            </Route>
+          </Route>
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
