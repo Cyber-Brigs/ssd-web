a28a6cafd4ffabd18b8345dcb441f3e0090f2925
steve <austines811@gmail.com>
2024-05-23 14:26:48 +0300

    implements view components with actions for srs document uploads

---
+++ src/components/texts/SrsUploadsTable.jsx
@@ -0,0 +1,179 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setCurrentUploadDetail } from "../../features/upload/uploadSlice.js";
+import CustomDataGrid from "../common/utils/CustomDataGrid.jsx";
+import { Box, Typography, IconButton, Menu, MenuItem } from "@mui/material";
+import moment from "moment";
+import Chip from "@mui/material/Chip";
+import { red, green } from "@mui/material/colors";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import PageviewIcon from "@mui/icons-material/Pageview";
+import MemoryIcon from "@mui/icons-material/Memory";
+import SrsDocPreviewModal from "./modals/SrsDocPreviewModal.jsx";
+import SrsProcessModal from "./modals/SrsProcessModal.jsx";
+const SrsUploadsTable = ({ fileData }) => {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [rowParams, setRowParams] = useState({});
+  const [openSrsDocPreviewModal, setOpenSrsDocPreviewModal] = useState(false);
+  const [openSrsProcessModal, setOpenSrsProcessModal] = useState(false);
+  const dispatch = useDispatch();
+  const handleUploadActionsClick = (params) => (event) => {
+    setRowParams(params.row);
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseMenu = () => {
+    setAnchorElNav(null);
+  };
+  const closeSrsDocPreviewModal = () => {
+    setOpenSrsDocPreviewModal(false);
+  };
+  const closeSrsProcessModal = () => {
+    setOpenSrsProcessModal(false);
+  };
+  const handleMenuItemClick = (prop) => {
+    handleCloseMenu();
+    dispatch(setCurrentUploadDetail({ currentUploadDetail: rowParams }));
+    if (prop === "view") {
+      setOpenSrsDocPreviewModal(true);
+    } else {
+      setOpenSrsProcessModal(true);
+    }
+  };
+  function getChipProps(params) {
+    if (params.value === "available") {
+      return {
+        label: "AVAILABLE",
+        style: {
+          width: "150px",
+          color: green[600],
+          borderColor: green[100],
+          backgroundColor: green[100],
+          borderRadius: 5,
+        },
+      };
+    } else {
+      return {
+        label: "UNAVAILABLE",
+        style: {
+          width: "200px",
+          color: red[600],
+          borderColor: red[100],
+          backgroundColor: red[100],
+          borderRadius: 5,
+        },
+      };
+    }
+  }
+
+  const columns = [
+    {
+      field: "document_name",
+      headerName: "Document Name",
+      width: 450,
+    },
+    {
+      field: "date_created",
+      headerName: "Upload Date",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <Typography>
+            {moment(params.row.date_created).format("DD-MM-YYYY")}
+          </Typography>
+        );
+      },
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 250,
+      renderCell: (params) => {
+        return (
+          <Chip variant="outlined" size="medium" {...getChipProps(params)} />
+        );
+      },
+    },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      width: 100,
+      renderCell: (params) => {
+        return (
+          <>
+            <IconButton onClick={handleUploadActionsClick(params)}>
+              <MoreVertIcon />
+            </IconButton>
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <>
+      <SrsDocPreviewModal
+        openSrsDocPreviewModal={openSrsDocPreviewModal}
+        closeSrsDocPreviewModal={closeSrsDocPreviewModal}
+      />
+      <SrsProcessModal
+        openSrsProcessModal={openSrsProcessModal}
+        closeSrsProcessModal={closeSrsProcessModal}
+      />
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorElNav}
+        anchorOrigin={{
+          vertical: "center",
+          horizontal: "center",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        open={Boolean(anchorElNav)}
+        onClose={handleCloseMenu}
+      >
+        <MenuItem onClick={() => handleMenuItemClick("view")}>
+          <Box display="flex" alignItems="center" textAlign="center">
+            <PageviewIcon
+              sx={{
+                color: `primary.main`,
+                mr: 1,
+                fontSize: "medium",
+              }}
+            />
+            Preview
+          </Box>
+        </MenuItem>
+        <MenuItem onClick={() => handleMenuItemClick("process")}>
+          <Box display="flex" alignItems="center" textAlign="center">
+            <MemoryIcon
+              sx={{
+                color: `primary.main`,
+                mr: 1,
+                fontSize: "medium",
+              }}
+            />
+            Preprocess
+          </Box>
+        </MenuItem>
+      </Menu>
+      <Box
+        sx={{
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: "primary.lightest_gray",
+            fontSize: 16,
+          },
+          marginRight: 2,
+          //   padding: 2,
+        }}
+      >
+        <CustomDataGrid rows={fileData} columns={columns} />
+      </Box>
+    </>
+  );
+};
+
+export default SrsUploadsTable;
---
+++ src/components/texts/UploadSrs.jsx
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from "react";
+import {
+  Stack,
+  Card,
+  CardHeader,
+  CardContent,
+  Box,
+  Typography,
+  Button,
+  IconButton,
+  LinearProgress,
+} from "@mui/material";
+import UploadIcon from "@mui/icons-material/Upload";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import { axiosApi } from "../../api";
+import { getUserSrsUploads } from "../../api/processing/textProcessing.js";
+import SrsUploadsTable from "./SrsUploadsTable.jsx";
+import DashView from "../users/DashView.jsx";
+const UploadSrs = () => {
+  const [file, setFile] = useState(null);
+  const [srsUploads, setSrsUploads] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+  const fetchFileUploads = () => {
+    getUserSrsUploads().then((res) => {
+      if (res.status === 200) {
+        setSrsUploads(res.data);
+        setLoading(false);
+      } else {
+        console.log("no");
+        setLoading(false);
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchFileUploads();
+  }, []);
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append("document", file);
+    try {
+      await axiosApi.post("/upload/", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      setFile(null);
+      alert("File uploaded successfully!");
+    } catch (error) {
+      console.error(error);
+      alert("Failed to upload file.");
+    }
+  };
+  
+  return (
+    <div className="bg-[#F5F5F5] p-3 right-side min-h-screen min-w-full ">
+      <div className="ml-60">
+        <DashView
+          data={"Software/System Requirement Specification Documents"}
+        />
+      </div>
+      <div className="ml-60">
+        <Card sx={{ m: 2 }}>
+          <CardHeader
+            title={
+              <Typography variant="h6" color="black" fontWeight="500">
+                Upload SRS Document
+              </Typography>
+            }
+            subheader={
+              <label for="file-upload" class="custom-file-upload">
+                <IconButton
+                  color="primary"
+                  aria-label="upload file"
+                  component="label"
+                  onChange={handleFileChange}
+                >
+                  <AddCircleOutlineIcon />
+                  <input
+                    accept=".pdf"
+                    type="file"
+                    style={{ display: "none" }}
+                  />
+                  <Typography
+                    sx={{
+                      border: "solid primary.main 2px",
+                      padding: "5px",
+                    }}
+                  >
+                    Browse Files
+                  </Typography>
+                </IconButton>
+              </label>
+            }
+            action={
+              file && (
+                <Button
+                  startIcon={<UploadIcon />}
+                  type="submit"
+                  variant="outlined"
+                  onClick={handleUpload}
+                >
+                  Upload Document
+                </Button>
+              )
+            }
+          />
+          {file && <CardContent>{file.name}</CardContent>}
+          <CardContent sx={{ p: "-2" }}>
+            <Typography variant="h6" color="grey" fontWeight="500">
+              User SRS Uploads
+            </Typography>
+            {loading && <LinearProgress />}
+            {!loading ? (
+              srsUploads.length > 0 ? (
+                <SrsUploadsTable fileData={srsUploads} />
+              ) : (
+                <Typography sx={{ color: "black" }}>
+                  <HelpOutlineIcon color="primary" />
+                  There are no srs documents uploaded yet. To get started, click
+                  Browse files above, and proceed to upload the SRS document.
+                </Typography>
+              )
+            ) : null}
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default UploadSrs;
