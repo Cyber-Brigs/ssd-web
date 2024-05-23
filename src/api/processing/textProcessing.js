bf80730d19b606a67d1500aaf1ab67171ab65852
steve <austines811@gmail.com>
2024-05-23 20:38:11 +0300

    adds apis for model similarities and viewing

--- src/api/processing/textProcessing.js
+++ src/api/processing/textProcessing.js
@@ -31,3 +31,11 @@ export const completeModelTraining = async (type, model_id, selected_topics) => 
     selected_topics: parseInt(selected_topics),
   });
 };
+
+export const getTopicModels = async (type) => {
+  return await textsApi.get(`/${type}-models-view/`);
+};
+
+export const getSimilarityResults = async (type, model_id, processing_id) => {
+  return await textsApi.patch(`/${type}-results/?pk=${model_id}}&text_processing_id=${processing_id}/`);
+};
