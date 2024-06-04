dd8ab8ed798c7e1d01e9c759884b56658716df3d
steve <austines811@gmail.com>
2024-06-05 00:48:49 +0300

    adds a footer copyright and placeholder image asset

---
+++ src/components/utilities/Copyright.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Box, Typography, Link } from "@mui/material";
+export default function Copyright() {
+  return (
+    <Box
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        right: 0,
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        mt: 2,
+        backgroundColor: "azure"
+      }}
+      component="footer"
+    >
+      <Typography variant="body2" color="black" align="center">
+        {"©  "} {new Date().getFullYear()}{" "}
+        
+        <Link color="#295FAB" href="https://www.nrf.go.ke/">
+          NRF Kenya
+        </Link>{". "}
+        {"All Rights Reserved."}
+      </Typography>
+    </Box>
+  );
+}
